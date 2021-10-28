1c40835d19868f3735c71de488d83ba91b66e676
kaitlynnchan <kaitlynnchan@outlook.com>
2021-10-27 23:50:41 -0700

    Modify animations
    Add animations to whole page to move.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     console.log("ready");
 
-    // day selected
+    // icons animation
     $(".icons a").hover(function(){
         $(this).children(".fa-circle").toggleClass("selected");
         $(this).children(".icon").toggleClass("selected");
         console.log("hover");
     });
 
+    // side links hover animations
     // $(".side-links a").hover(
     //     function(){
     //         $(this).parent().children(".overlay").css("visibility", "visible");
@@ -19,25 +20,39 @@ $(document).ready(function(){
     //     }
     // );
 
-    $(".side-links.pull-right a").hover(
-        function(){
-            $(this).parent().addClass("animate__animated animate__slideInRight");
-            console.log("side links visible");
-        }
-    );
-    $(".side-links.pull-left a").hover(
-        function(){
-            $(this).parent().addClass("animate__animated animate__slideInLeft");
-            console.log("side links visible");
-        }
-    );
+    // $(".side-links.pull-right").on("animationend", function(){
+    //     console.log("animation end");
+    //     $(this).removeClass("animate__animated animate__slideInRight");
+    // });
+    // $(".side-links.pull-left").on("animationend", function(){
+    //     console.log("animation end");
+    //     $(this).removeClass("animate__animated animate__slideInLeft");
+    // });
 
-    $(".side-links.pull-right").on("animationend", function(){
-        console.log("animation end");
-        $(this).removeClass("animate__animated animate__slideInRight");
+    // side links click animation
+    $(".side-links a").on("click", function(){
+        if($(this).parent().hasClass("pull-left")){
+            $(this).parent().parent().addClass("animate__animated animate__slideOutRight");
+            console.log("pull-left");
+        } else if($(this).parent().hasClass("pull-right")){
+            $(this).parent().parent().addClass("animate__animated animate__slideOutLeft");
+            console.log("pull-right");
+        }
+        console.log("clicked");
     });
-    $(".side-links.pull-left").on("animationend", function(){
+    
+    $(".whole-home-page").on("animationend", function(){
         console.log("animation end");
-        $(this).removeClass("animate__animated animate__slideInLeft");
+        if($(this).hasClass("animate__slideOutLeft")){
+            $(this).parent().children(".animate__slideOutLeft").removeClass("animate__animated animate__slideOutLeft");
+            $(this).css("visibility", "hidden");
+            console.log("yup to out left");
+        } else if($(this).hasClass("animate__slideOutRight")){
+            $(this).parent().children(".animate__slideOutRight").removeClass("animate__animated animate__slideOutRight");
+            $(this).css("visibility", "hidden");
+            console.log("yes to out right");
+        } else {
+            console.log("nope");
+        }
     });
 });
