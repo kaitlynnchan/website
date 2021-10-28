c3a686287cfa3c082276d5aded1ed280e859d634
kaitlynnchan <kaitlynnchan@outlook.com>
2021-10-28 11:17:07 -0700

    Create about page and add animation.
    Created basic layout for about page. Add animation
    slide in animation.

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
     // side links click animation
     $(".side-links a").on("click", function(){
         if($(this).parent().hasClass("pull-left")){
+            $(this).parent().parent().parent().children(".about").css("display", "flex");
             $(this).parent().parent().addClass("animate__animated animate__slideOutRight");
+            $(this).parent().parent().parent().children(".about").addClass("animate__animated animate__slideInLeft");
             console.log("pull-left");
         } else if($(this).parent().hasClass("pull-right")){
             $(this).parent().parent().addClass("animate__animated animate__slideOutLeft");
@@ -34,11 +36,13 @@ $(document).ready(function(){
         console.log("animation end");
         if($(this).hasClass("animate__slideOutLeft")){
             $(this).parent().children(".animate__slideOutLeft").removeClass("animate__animated animate__slideOutLeft");
-            $(this).css("visibility", "hidden");
+            $(this).css("display", "none");
+            // $(this).parent().children(".about").css("display", "flex");
             console.log("yup to out left");
         } else if($(this).hasClass("animate__slideOutRight")){
             $(this).parent().children(".animate__slideOutRight").removeClass("animate__animated animate__slideOutRight");
-            $(this).css("visibility", "hidden");
+            $(this).css("display", "none");
+            // $(this).parent().children(".about").css("display", "flex");
             console.log("yes to out right");
         } else {
             console.log("nope");
