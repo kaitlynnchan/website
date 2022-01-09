033712bafb976a8be1413fa67a993ce7844c4ae2
kaitlynnchan <kaitlynnchan@outlook.com>
2022-01-08 23:03:36 -0800

    Add hover animations to navigation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -9,55 +9,37 @@ $(document).ready(function(){
         console.log("Icon hover");
     });
 
-    // // top-btn animation
-    // $(".top-btn a").hover(function(){
-    //     $(this).children("span").toggleClass("selected");
-    // });
-
-
-    // side button click animation
-    $("#home-page .side-btn a").on("click", function(){
+    // clicking page buttons
+    function clickBtn(btn, pageName){
         if(isAnimationDone){
             $("body").css("overflow", "hidden");
-            const $sideBtn = $(this).parents("section");
-            if($sideBtn.hasClass("pull-left")){
-                // open about page
-                $sideBtn.parents(".page").addClass("animate__animated animate__slideOutRight");
-                $("#about-page").css("display", "flex");
-                $("#about-page").addClass("animate__animated animate__slideInLeft");
-            } else if($sideBtn.hasClass("pull-right")){
-                // open projects page
-                $sideBtn.parents(".page").addClass("animate__animated animate__slideOutLeft");
-                $("#projects-page").css("display", "flex");
-                $("#projects-page").addClass("animate__animated animate__slideInRight");
+            if(btn.hasClass("pull-left")){
+                // moving right
+                btn.parents(".page").addClass("animate__animated animate__slideOutRight");
+                $(pageName).css("display", "flex");
+                $(pageName).addClass("animate__animated animate__slideInLeft");
+            } else if(btn.hasClass("pull-right")){
+                // moving left
+                btn.parents(".page").addClass("animate__animated animate__slideOutLeft");
+                $(pageName).css("display", "flex");
+                $(pageName).addClass("animate__animated animate__slideInRight");
             }
             isAnimationDone = false;
         }
+    }
+
+    // side button click animation
+    $("#home-page .side-btn a").on("click", function(){
+        const $sideBtn = $(this).parents("section");
+        if($sideBtn.hasClass("pull-left")){
+            // open about page
+            clickBtn($sideBtn, "#about-page");
+        } else if($sideBtn.hasClass("pull-right")){
+            // open projects page
+            clickBtn($sideBtn, "#projects-page");
+        }
         console.log("Clicked side button");
     });
-
-    // back button animation
-    // function clickBackBtn(){
-    //     if(isAnimationDone){
-    //         $("body").css("overflow", "hidden");
-    //         const $btn = $(this).parents("section");
-    //         if($btn.hasClass("pull-left")){
-    //             // moving right
-    //             $btn.parents(".page").addClass("animate__animated animate__slideOutRight");
-    //             $("#home-page").css("display", "flex");
-    //             $("#home-page").addClass("animate__animated animate__slideInLeft");
-    //         } else if($btn.hasClass("pull-right")){
-    //             // moving left
-    //             $btn.parents(".page").addClass("animate__animated animate__slideOutLeft");
-    //             $("#home-page").css("display", "flex");
-    //             $("#home-page").addClass("animate__animated animate__slideInRight");
-    //         }
-    //         isAnimationDone = false;
-    //     }
-    //     console.log("Clicked back button");
-    // }
-    // $(".side-btn.back a").on("click", clickBackBtn);
-    // $(".top-btn.back a").on("click", clickBackBtn);
 
     // animation end
     function removeAnimations(){
@@ -89,10 +71,14 @@ $(document).ready(function(){
     $(".side-btn").on("animationend", removeAnimations);
 
 
+    /** 
+     * Navigation Buttons
+     */
+    // open and collapse navigation menu
     function toggleNav(){
         var pageName = $(this).parents(".page").attr("id");
-        $(this).parent().find(".nav-btns").toggleClass("dropped");
-        $(this).parent().find(".nav-btns").toggleClass("collapsed");
+        $(".nav-btns").toggleClass("dropped");
+        $(".nav-btns").toggleClass("collapsed");
         if(pageName == 'about-page'){
             $(".nav-btns").find("#about").addClass("page-open");
             $(".nav-btns").find("#projects").removeClass("page-open");
@@ -103,52 +89,48 @@ $(document).ready(function(){
     }
     $(".nav-hamburger").on("click", toggleNav);
 
-
-    function clickBtn(btn, pageName){
-        if(isAnimationDone){
-            $("body").css("overflow", "hidden");
-            // const $btn = $(btn).parents("section");
-            if(btn.hasClass("pull-left")){
-                // moving right
-                btn.parents(".page").addClass("animate__animated animate__slideOutRight");
-                $(pageName).css("display", "flex");
-                $(pageName).addClass("animate__animated animate__slideInLeft");
-            } else if(btn.hasClass("pull-right")){
-                // moving left
-                btn.parents(".page").addClass("animate__animated animate__slideOutLeft");
-                $(pageName).css("display", "flex");
-                $(pageName).addClass("animate__animated animate__slideInRight");
-            }
-            isAnimationDone = false;
-        }
-    }
-    function clickNavBtn(){
+    // click navigation buttons
+    $(".nav-btns a").on("click", function(){
         var linkName = $(this).attr("id");
+        const $btn = $(this).parents("section");
         if(linkName == 'home'){
-            const $btn = $(this).parents("section");
             clickBtn($btn, "#home-page");
             toggleNav();
         } else if(linkName == "about"){
-            const $btn = $(this).parents("section");
             clickBtn($btn, "#about-page");
             toggleNav();
         } else if(linkName == "projects"){
-            const $btn = $(this).parents("section");
             clickBtn($btn, "#projects-page");
             toggleNav();
         }
         console.log("Clicked back button");
-    }
-    $(".nav-btns a").on("click", clickNavBtn);
+    });
+
+    // click heading button
     $(".header .heading a").on("click", function(){
         const $btn = $(this).parents(".header").find(".nav-menu");
         clickBtn($btn, "#home-page");
     });
 
-    // top-btn animation
+    // heading button animation
     $(".heading a").hover(function(){
         $(this).children("h1").toggleClass("selected");
     });
+
+    $("a.nav-hamburger").hover(
+        function(){
+            $(this).children(".fa-circle").addClass("selected");
+            $(this).children(".fa-bars").addClass("selected");
+            console.log("Icon hover over");
+        },
+        function(){
+            if($(".nav-btns").hasClass("collapsed")){
+                $(this).children(".fa-circle").removeClass("selected");
+                $(this).children(".fa-bars").removeClass("selected");
+            }
+            console.log("Icon hover out");
+        }
+    );
 
     // $.ajax({
     //     url: "https://github.com/kaitlynnchan/website",
--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,17 +1,24 @@
 
 // detect size of screen
 function checkSize(){
-    if(window.matchMedia("only screen and (max-width: 1000px)").matches){    
+    if(window.matchMedia("only screen and (max-width: 1000px)").matches){
+        // screen size is less than 1000px
         // changed location of side buttons
         $("#home-page .side-btn").insertBefore($("#home-page .body .contact-info"));
         $("#home-page .side-btn").addClass("center");
         $("#home-page .inner.box h1").css("font-size", "2.75rem")
         $("#home-page .body .inner.top").css("display", "none");
+
+        // change navigation buttons location
+        $(".nav-menu .nav-btns").removeClass("horizontal");
     } else{
         $("#home-page").append($("#home-page .side-btn"));
         $("#home-page .side-btn").removeClass("center");
         $("#home-page .inner.box h1").css("font-size", "3.75rem")
         $("#home-page .body .inner.top").css("display", "flex");
+        
+        // change navigation buttons location
+        $(".nav-menu .nav-btns").addClass("horizontal");
     }
 }
 
