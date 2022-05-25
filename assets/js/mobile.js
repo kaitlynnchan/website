29d720c33641fd31d108a7b1152bbf11d4f608f4
kaitlynnchan <kaitlynnchan@outlook.com>
2022-05-24 23:23:56 -0700

    Temp Commit

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -41,4 +41,35 @@ checkSize();
 $(window).resize(function(){
     console.log("resized");
     checkSize();
-})
+});
+
+$(document).ready(function(){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = ($( window ).height() / 3) * 2,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$nav_bar = $('.top-nav');
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > offset ){
+            $nav_bar.addClass('visible');
+            $nav_bar.removeClass('fade-out');
+            // $nav_bar.removeClass('cd-is-visible cd-fade-out');
+        } else{
+            $nav_bar.addClass('fade-out');
+            $nav_bar.removeClass('visible');
+        }
+        if($(this).scrollTop() + $(window).height() == $(document).height()){
+            console.log("bottom");
+            $("#projects-page .body").append($(".top-nav .contact-info"));
+        } 
+        if($(this).scrollTop() == $(document).height() - ($(window).height()/2)){
+            console.log("here")
+            $("#projects-page .body .contact-info").insertBefore($(".top-nav"));
+        }
+		// if( $(this).scrollTop() > offset_opacity ) { 
+		// }
+	});
+});
