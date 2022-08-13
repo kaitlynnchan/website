3a34a5875d2ef49c6bea0f4cb95928e921714e85
kaitlynnchan <kaitlynnchan@outlook.com>
2022-08-13 10:55:45 -0700

    Refactor js files
    Removed unused code and reorganized code.

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     console.log("ready");
     var isAnimationDone = false;
 
+    /**
+     * Icons
+     */
     // icons animation
     $(".icons a").hover(function(){
         $(this).children(".fa-circle").toggleClass("selected");
@@ -9,66 +12,15 @@ $(document).ready(function(){
         console.log("Icon hover");
     });
 
-    // animation end
-    // function removeAnimations(){
-    //     console.log("Animation end on '" + $(this).attr("class") + "'");
-    //     if($(this).hasClass("animate__slideOutLeft")){
-    //         $(this).removeClass("animate__animated animate__slideOutLeft");
-    //         $(this).css("display", "none");
-    //         $("body").css("overflow", "");
-    //     } 
-    //     else if($(this).hasClass("animate__slideOutRight")){
-    //         $(this).removeClass("animate__animated animate__slideOutRight");
-    //         $(this).css("display", "none");
-    //         $("body").css("overflow", "");
-    //     } 
-    //     else if($(this).hasClass("animate__slideInLeft")){
-    //         $(this).removeClass("animate__animated animate__slideInLeft");
-    //     } 
-    //     else if($(this).hasClass("animate__slideInRight")){
-    //         $(this).removeClass("animate__animated animate__slideInRight");
-    //     } 
-    //     else{
-    //         console.log("No animations removed");
-    //     }
-    //     isAnimationDone = true;
-    // }
-    // $("#home-page").on("animationend", removeAnimations);
-    // $("#about-page").on("animationend", removeAnimations);
-    // $("#projects-page").on("animationend", removeAnimations);
-    // $(".side-nav").on("animationend", removeAnimations);
-
-
-    /** 
-     * Navigation Buttons
+    /**
+     * Clicking Nav Buttons
      */
-    // clicking page buttons animation
-    // function clickBtn(btnContainer, pageName){
-    //     if(isAnimationDone){
-    //         $("body").css("overflow", "hidden");
-    //         if(btnContainer.hasClass("pull-left")){
-    //             // moving right
-    //             btnContainer.parents(".page").addClass("animate__animated animate__slideOutRight");
-    //             $(pageName).css("display", "flex");
-    //             $(pageName).addClass("animate__animated animate__slideInLeft");
-    //         } else if(btnContainer.hasClass("pull-right")){
-    //             // moving left
-    //             btnContainer.parents(".page").addClass("animate__animated animate__slideOutLeft");
-    //             $(pageName).css("display", "flex");
-    //             $(pageName).addClass("animate__animated animate__slideInRight");
-    //         }
-    //         isAnimationDone = false;
-    //     }
-    // }
-
     // returns string to page identifier
     function convertToPageID(pageName){
         return "#" + pageName + "-page";
     }
 
-    var animating = false;
-    // side navigation
-    $(".side-nav a").on("click", function(e){
+    function clickNavButton(e){
         var linkName = $(this).attr("id");
         let pageName = convertToPageID(linkName);
         let dist = $(pageName).offset().top;
@@ -78,33 +30,29 @@ $(document).ready(function(){
         );
         console.log("Clicked side navigation");
         return false;
-    });
+    }
 
+    /**
+     * Side Navigation
+     */
+    $(".side-nav a").on("click", clickNavButton);
+
+    /**
+     * Side Navigation Position Bar
+     */
+    $(".side-nav-bar a").on("click", clickNavButton);
     
-    $(".side-nav-bar a").on("click", function(e){
-        var linkName = $(this).attr("id");
-        let pageName = convertToPageID(linkName);
-        let dist = $(pageName).offset().top;
-        e.preventDefault();
-        $("html, body").stop().animate(
-            {scrollTop: dist}, 800
-        );
-        console.log("Clicked side navigation");
-        return false;
+    // show text
+    $(".side-nav-bar").hover(function(){
+        $(".side-nav-bar ul li a div").toggleClass("show");
+        $(".side-nav-bar ul li a div").addClass("animate__fadeIn");
+        
     });
 
-    // top navigation menu
-    $(".top-nav .nav-menu a").on("click", function(){
-        const $btn = $(this).parents("section");
-        var linkName = $(this).attr("id");        
-        // clickBtn($btn, convertToPageID(linkName));
-
-        // trigger event handler for navigation hamburger
-        $(this).parents("section").find(".nav-hamburger")
-            .trigger("click")
-            .trigger("mouseleave");
-        console.log("Clicked top nav menu");
-    });
+    /**
+     * Top Navigation Menu
+     */ 
+    $(".top-nav .nav-menu a").on("click", clickNavButton);
 
     // open and close top navigation menu
     $(".nav-hamburger").on("click", function(){
@@ -153,107 +101,54 @@ $(document).ready(function(){
         }
     });
 
-
-    // top navigation heading
-    // $(".heading a").on("click", function(){
-    //     const $topNavBtn = $(this).parents(".header").find(".top-nav");
-    //     clickBtn($topNavBtn, convertToPageID("home"));
-    // });
-
-    // top navigation heading hover animation
-    // $(".heading a").hover(function(){
-    //     $(this).children("h1").toggleClass("selected");
-    // });
-
-    // showNavBar();
-
-    // side navigation bar
-    $(".side-nav-bar").hover(function(){
-        $(".side-nav-bar ul li a div").toggleClass("show");
-        $(".side-nav-bar ul li a div").addClass("animate__fadeIn");
-        
-    });
-
-
-
-    // browser window scroll (in pixels) after which the "back to top" link is shown
-    var offset = ($( window ).height() / 3) * 2,
-        //grab the "back to top" link
-        $nav_bar = $('.top-nav');
-    $(window).scroll(function(){
-        if( $(this).scrollTop() > offset ){
-            $nav_bar.addClass('visible');
-            $nav_bar.removeClass('fade-out');
-            
-            // fade out contact info in top nav when page is at bottom
-            var contact_info_height = $("#projects-page .body .contact-info").height();
-            if($(this).scrollTop() + $(window).height() >= $(document).height() - contact_info_height){
-                $nav_bar.children(".contact-info").addClass('fade-out');
-                $nav_bar.children(".contact-info").removeClass('visible');
-            } else{
-                $nav_bar.children(".contact-info").addClass('visible');
-                $nav_bar.children(".contact-info").removeClass('fade-out');
-            }
-        } else{
-            $nav_bar.addClass('fade-out');
-            $nav_bar.removeClass('visible');
-            $nav_bar.children(".contact-info").removeClass('visible');
-        }
-
-
-        /**
-         * Side Navigation Bar
-         */
-        if( $("#projects-page").position().top - $(this).scrollTop() < $(window).height() / 2){
-            $(".side-nav-bar ul li a.projects").addClass("selected");
-            $(".side-nav-bar ul li a.about").removeClass("selected");
-            $(".side-nav-bar ul li a.home").removeClass("selected");
-        } else if ( $("#about-page").position().top - $(this).scrollTop() < $(window).height() / 2){
-            $(".side-nav-bar ul li a.about").addClass("selected");
-            $(".side-nav-bar ul li a.home").removeClass("selected");
-            $(".side-nav-bar ul li a.projects").removeClass("selected");
-        } else {
-            $(".side-nav-bar ul li a.home").addClass("selected");
-            $(".side-nav-bar ul li a.about").removeClass("selected");
-            $(".side-nav-bar ul li a.projects").removeClass("selected");
-        }
-    });
-
 });
 
-// function showNavBar(){
-//     var pagePos = $("#about-page").position();
-//     if(pagePos.top <= 0){
-//         console.log(pagePos);
-//     }
-// }
+
+/**
+ * Scrolling
+ */
+$(window).scroll(function(){
+    /**
+     * Top Navigation Menu
+     */
+    var winHeight = $(window).height();
+    var offset = (winHeight / 3) * 2;
+    var $nav_bar = $('.top-nav');
+    if( $(this).scrollTop() > offset ){
+        $nav_bar.addClass('visible');
+        $nav_bar.removeClass('fade-out');
+        
+        // fade out contact info in top nav when page is at bottom
+        var contact_info_height = $("#projects-page .body .contact-info").height();
+        if($(this).scrollTop() + $(window).height() >= $(document).height() - contact_info_height){
+            $nav_bar.children(".contact-info").addClass('fade-out');
+            $nav_bar.children(".contact-info").removeClass('visible');
+        } else{
+            $nav_bar.children(".contact-info").addClass('visible');
+            $nav_bar.children(".contact-info").removeClass('fade-out');
+        }
+    } else{
+        $nav_bar.addClass('fade-out');
+        $nav_bar.removeClass('visible');
+        $nav_bar.children(".contact-info").removeClass('visible');
+    }
 
 
-// jQuery(document).ready(function($){
-// 	// browser window scroll (in pixels) after which the "back to top" link is shown
-// 	var offset = 300,
-// 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-// 		offset_opacity = 1200,
-// 		//duration of the top scrolling animation (in ms)
-// 		scroll_top_duration = 700,
-// 		//grab the "back to top" link
-// 		$back_to_top = $('.cd-top');
-
-// 	//hide or show the "back to top" link
-// 	$(window).scroll(function(){
-// 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-// 		if( $(this).scrollTop() > offset_opacity ) { 
-// 			$back_to_top.addClass('cd-fade-out');
-// 		}
-// 	});
-
-// 	//smooth scroll to top
-// 	$back_to_top.on('click', function(event){
-// 		event.preventDefault();
-// 		$('body,html').animate({
-// 			scrollTop: 0 ,
-// 		 	}, scroll_top_duration
-// 		);
-// 	});
-
-// });
+    /**
+     * Side Navigation Bar
+     */
+    var windowMidPos = winHeight / 2;
+    if( $("#projects-page").position().top - $(this).scrollTop() < windowMidPos){
+        $(".side-nav-bar ul li a.projects").addClass("selected");
+        $(".side-nav-bar ul li a.about").removeClass("selected");
+        $(".side-nav-bar ul li a.home").removeClass("selected");
+    } else if ( $("#about-page").position().top - $(this).scrollTop() < windowMidPos){
+        $(".side-nav-bar ul li a.about").addClass("selected");
+        $(".side-nav-bar ul li a.home").removeClass("selected");
+        $(".side-nav-bar ul li a.projects").removeClass("selected");
+    } else {
+        $(".side-nav-bar ul li a.home").addClass("selected");
+        $(".side-nav-bar ul li a.about").removeClass("selected");
+        $(".side-nav-bar ul li a.projects").removeClass("selected");
+    }
+});
