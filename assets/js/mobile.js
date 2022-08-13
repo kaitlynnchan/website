59fede8ba59d090386f2e6276ee0a65d2565658e
kaitlynnchan <kaitlynnchan@outlook.com>
2022-08-13 16:32:41 -0700

    Mobile friendly design

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -2,37 +2,37 @@
 // detect size of screen
 function checkSize(){
     // change when screen size is less than 1000px
-    if(window.matchMedia("only screen and (max-width: 1000px)").matches){
+    if(window.matchMedia("only screen and (max-width: 1100px)").matches){
         // changed location of side buttons
-        $("#home-page .side-nav").insertBefore($("#home-page .body .contact-info"));
+        $("#home-page .content").css("flex-direction", "column");
         $("#home-page .side-nav").addClass("center");
+        $("#home-page .nav-btns").addClass("center");
+
         $("#home-page .inner.box h1").css("font-size", "2.75rem");
-        $("#home-page .body .inner.top").css("display", "none");
 
         // change navigation buttons layout
         $(".top-nav .nav-menu").removeClass("horizontal");
+        $(".top-nav .contact-info").addClass("hide");
     } else{
-        // $("#home-page").append($("#home-page .side-nav"));
-        // $("#home-page .side-nav").removeClass("center");
+        $("#home-page .content").css("flex-direction", "row");
+        $("#home-page .side-nav").removeClass("center");
+        $("#home-page .nav-btns").removeClass("center");
+
         $("#home-page .inner.box h1").css("font-size", "3.75rem");
-        $("#home-page .body .inner.top").css("display", "flex");
         
         // undo navigation buttons layout
         $(".top-nav .nav-menu").addClass("horizontal");
+        $(".top-nav .contact-info").removeClass("hide");
     }
 
     // change when screen size is less than 750px
     if(window.matchMedia("only screen and (max-width: 750px)").matches){
         // change location of about title
-        $("#about-page .title").insertBefore($("#about-page .body .inner").first());
-        $("#about-page .title").addClass("inner center");
-        $("#about-page .inner .text").addClass("center");
+        $(".title").addClass("center");
 
     } else{
         // remove changes
-        // $("#about-page .body .inner").first().append($("#about-page .title"));
-        $("#about-page .title").removeClass("inner center");
-        $("#about-page .inner .text").removeClass("center");
+        $(".title").removeClass("center");
     }
 }
 
